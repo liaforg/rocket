cafa16178b41d719353ecc7d415223e5f0d07902
Lia Forgione <liaforg@gmail.com>
2017-06-22 13:48:37 -0400

    tests for 1 and 2

--- rocket_test.rb
+++ rocket_test.rb
@@ -2,5 +2,40 @@ require "minitest/autorun"
 require_relative "rocket"
 
 class RocketTest < Minitest::Test
-  # Write your tests here!
+
+def setup
+  @rocket = Rocket.new
+end
+
+def test_new_name
+  @rocket.name = ("John")
+  assert @rocket.name = "John"
+end
+
+def test_new_name_different
+  @rocket.name = ("John")
+  refute @rocket.name == "hello"
+end
+
+def test_new_colour
+  @rocket.colour = ("Red")
+  assert @rocket.colour = "Red"
+end
+
+def test_new_colour_different
+  @rocket.colour = (1)
+  refute @rocket.colour == "Red"
+end
+
+def test_lift_off
+  @rocket.flying? == false
+  assert @rocket.flying? == false
+end
+
+def test_lift_off_different
+  @rocket.flying?
+  refute @rocket.flying?
+end
+
+
 end
